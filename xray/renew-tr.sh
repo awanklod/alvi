60c49048b21d6cfc3ff14f132fea1647e6b3907a
awanklod <137737752+awanklod@users.noreply.github.com>
2023-07-30 01:34:57 -1200

    Update renew-tr.sh

--- xray/renew-tr.sh
+++ xray/renew-tr.sh
@@ -12,54 +12,77 @@ red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 
 
 clear
-NUMBER_OF_CLIENTS=$(grep -c -E "^#! " "/etc/xray/config.json")
-	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
-		clear
-        echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-        echo -e "\E[40;1;37m          ⇱ Renew Trojan ⇲         \E[0m"
-        echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-		echo ""
-		echo "You have no existing clients!"
-		echo ""
-		echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-        echo ""
-        read -n 1 -s -r -p "Press any key to back on menu"
-        menu
-	fi
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+CEKEXPIRED () {
+today=$(date -d +1day +%Y-%m-%d)
+Exp1=$(curl -sS https://raw.githubusercontent.com/awanklod/izin_alvi/main/izin | grep $MYIP | awk '{print $3}')
+if [[ $today < $Exp1 ]]; then
+echo -e "\e[32mSTATUS SCRIPT AKTIF...\e[0m"
+else
+echo -e "\e[31mSCRIPT ANDA EXPIRED!\e[0m";
+exit 0
+fi
+}
+IZIN=$(curl -sS https://raw.githubusercontent.com/awanklod/izin_alvi/main/izin | awk '{print $4}' | grep $MYIP)
+if [ $MYIP = $IZIN ]; then
+echo -e "\e[32mPermission Accepted...\e[0m"
+CEKEXPIRED
+else
+echo -e "\e[31mPermission Denied!\e[0m";
+exit 0
+fi
 
-	clear
-	echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-    echo -e "\E[40;1;37m          ⇱ Renew Trojan ⇲         \E[0m"
-    echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-    echo ""
-  	grep -E "^#! " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
-    echo ""
-    red "tap enter to go back"
-    echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-	read -rp "Input Username : " user
-    if [ -z $user ]; then
-    menu
-    else
-    read -p "Expired (days): " masaaktif
-    exp=$(grep -wE "^#! $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
-    now=$(date +%Y-%m-%d)
-    d1=$(date -d "$exp" +%s)
-    d2=$(date -d "$now" +%s)
-    exp2=$(( (d1 - d2) / 86400 ))
-    exp3=$(($exp2 + $masaaktif))
-    exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
-    sed -i "/#! $user/c\#! $user $exp4" /etc/xray/config.json
-    systemctl restart xray > /dev/null 2>&1
-    clear
-    echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-    echo " Trojan Account Was Successfully Renewed"
-    echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-    echo ""
-    echo " Client Name : $user"
-    echo " Expired On  : $exp4"
-    echo ""
-    echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-    echo ""
-    read -n 1 -s -r -p "Press any key to back on menu"
-    menu
-    fi
+clear
+NUMBER_OF_CLIENTS=$(grep -c -E "^#! " "/etc/xray/config.json")
+if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+clear
+echo -e "${CYAN}╒════════════════════════════════════════╕${NC}"
+echo -e "${BIWhite}           ⇱ RENEW USER TROJAN ⇲         ${NC}"
+echo -e "${CYAN}╘════════════════════════════════════════╛${NC}"
+echo ""
+echo "You have no existing clients!"
+echo ""
+echo -e "${CYAN}══════════════════════════════════════════${NC}"
+echo ""
+read -n 1 -s -r -p "Press any key to back on menu"
+menu-trojan
+fi
+
+clear
+echo -e "${CYAN}╒════════════════════════════════════════╕${NC}"
+echo -e "${BIWhite}           ⇱ RENEW USER TROJAN ⇲         ${NC}"
+echo -e "${CYAN}╘════════════════════════════════════════╛${NC}"
+echo ""
+grep -E "^#! " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
+echo ""
+red "tap enter to go back"
+echo -e "${CYAN}══════════════════════════════════════════${NC}"
+read -rp "Input Username : " user
+if [ -z $user ]; then
+menu-trojan
+else
+read -p "Expired (days): " masaaktif
+exp=$(grep -wE "^#! $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+now=$(date +%Y-%m-%d)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+exp3=$(($exp2 + $masaaktif))
+exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
+sed -i "/#! $user/c\#! $user $exp4" /etc/xray/config.json
+systemctl restart xray > /dev/null 2>&1
+clear
+echo -e "${CYAN}══════════════════════════════════════════${NC}"
+echo " Trojan Account Was Successfully Renewed"
+echo -e "${CYAN}══════════════════════════════════════════${NC}"
+echo ""
+echo " Client Name : $user"
+echo " Expired On  : $exp4"
+echo ""
+echo -e "${CYAN}══════════════════════════════════════════${NC}"
+echo ""
+read -n 1 -s -r -p "Press any key to back on menu"
+menu-trojan
+fi
