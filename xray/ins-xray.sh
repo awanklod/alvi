33e587db45bf9af999cb2ccf1f82bf2261b92fe1
awanklod <137737752+awanklod@users.noreply.github.com>
2023-07-29 19:01:47 -1200

    Update ins-xray.sh

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -75,40 +75,6 @@ chmod +x /usr/local/bin/ssl_renew.sh
 if ! grep -q 'ssl_renew.sh' /var/spool/cron/crontabs/root;then (crontab -l;echo "15 03 */3 * * /usr/local/bin/ssl_renew.sh") | crontab;fi
 
 mkdir -p /home/vps/public_html
-
-# make folder xray 
-rm -rf /etc/vmess/.vmess.db
-rm -rf /etc/vless/.vless.db
-rm -rf /etc/trojan/.trojan.db
-rm -rf /etc/shadowsocks/.shadowsocks.db
-mkdir -p /etc/xray
-mkdir -p /etc/vmess
-mkdir -p /etc/vless
-mkdir -p /etc/trojan
-mkdir -p /etc/shadowsocks
-mkdir -p /usr/bin/xray/
-mkdir -p /var/log/xray/
-mkdir -p /var/www/html
-mkdir -p /etc/kyt/limit/vmess/ip
-mkdir -p /etc/kyt/limit/vless/ip
-mkdir -p /etc/kyt/limit/trojan/ip
-mkdir -p /etc/kyt/limit/shadowsocks/ip
-mkdir -p /etc/limit/vmess
-mkdir -p /etc/limit/vless
-mkdir -p /etc/limit/trojan
-mkdir -p /etc/limit/shadowsocks
-chmod +x /var/log/xray
-touch /etc/xray/domain
-touch /var/log/xray/access.log
-touch /var/log/xray/error.log
-touch /etc/vmess/.vmess.db
-touch /etc/vless/.vless.db
-touch /etc/trojan/.trojan.db
-touch /etc/shadowsocks/.shadowsocks.db
-echo "& plughin Account" >>/etc/vmess/.vmess.db
-echo "& plughin Account" >>/etc/vless/.vless.db
-echo "& plughin Account" >>/etc/trojan/.trojan.db
-echo "& plughin Account" >>/etc/shadowsocks/.shadowsocks.db
     
 # set uuid
 uuid=$(cat /proc/sys/kernel/random/uuid)
