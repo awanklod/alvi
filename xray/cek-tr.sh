41e3edb676ee046dde143692b8ef6e6d6a338581
awanklod <137737752+awanklod@users.noreply.github.com>
2023-07-31 01:58:38 -1200

    Update cek-tr.sh

--- xray/cek-tr.sh
+++ xray/cek-tr.sh
@@ -57,7 +57,7 @@ if [[ -z "$jum" ]]; then
 echo > /dev/null
 else
 iplimit=$(cat /etc/kyt/limit/trojan/ip/${akun})
-jum2=$(cat /tmp/iptrojan.txt | wc -l)
+jum2=$(cat /tmp/iptrojan.txt | nl)
 byte=$(cat /etc/trojan/${akun})
 lim=$(con ${byte})
 wey=$(cat /etc/limit/trojan/${akun})
@@ -67,7 +67,8 @@ echo -e " \033[1;36m╒═══════════════════
 printf "  %-13s %-7s %-8s %2s\n"   "  USEENAME : ${akun}" | lolcat
 printf "  %-13s %-7s %-8s %2s\n" "  LOGIN    : $lastlogin" | lolcat 
 printf "  %-13s %-7s %-8s %2s\n" "  LIMIT GB : ${gb}/${lim}" | lolcat  
-printf "  %-13s %-7s %-8s %2s\n" "  LIMIT IP : $jum2/$iplimit" | lolcat;
+printf "  %-13s %-7s %-8s %2s\n" "  LIMIT IP : ${iplimit}" | lolcat;
+printf "  %-13s %-7s %-8s %2s\n" "  LOGIN IP : ${jum2}" | lolcat;
 echo -e " \033[1;36m╘════════════════════════════════════════╛\033[0m"
 fi 
 rm -rf /tmp/iptrojan.txt
