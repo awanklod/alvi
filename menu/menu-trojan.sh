311e8b254c6339e9e62aeac499567d1e65b262c1
awanklod <137737752+awanklod@users.noreply.github.com>
2023-08-01 02:37:09 -1200

    Update menu-trojan.sh

--- menu/menu-trojan.sh
+++ menu/menu-trojan.sh
@@ -119,6 +119,7 @@ echo -e "     ${BICyan}[${BIWhite}03${BICyan}] Extending Account Trojan Active L
 echo -e "     ${BICyan}[${BIWhite}04${BICyan}] Delete Account Trojan     "
 echo -e "     ${BICyan}[${BIWhite}05${BICyan}] Check User Login Trojan     "
 echo -e "     ${BICyan}[${BIWhite}06${BICyan}] Check User Trojan     "
+echo -e "     ${BICyan}[${BIWhite}07${BICyan}] Check User Trojan 02     "
 
 echo -e " ${BICyan}└─────────────────────────────────────────────────────┘${NC}"
 echo -e "     ${BIYellow}Press x or [ Ctrl+C ] • To-${BIWhite}Exit${NC}"
@@ -132,6 +133,8 @@ case $opt in
 4) clear ; del-tr ;;
 5) clear ; cek-tr ;;
 6) clear ; cek-tr2 ;;
+7) clear ; cek-tr3 ;;
+
 0) clear ; menu ;;
 x) exit ;;
 *) echo "Boh salah tekan, Sayang kedak Babi" ; sleep 1 ; menu-trojan ;;
