d88ff7546db6b5a4173e38340ea5ce60878312d6
awanklod <137737752+awanklod@users.noreply.github.com>
2023-08-03 04:56:33 -1200

    Update xp.sh

--- ssh/xp.sh
+++ ssh/xp.sh
@@ -20,6 +20,7 @@ sed -i "/^### $user $exp/,/^},{/d" /etc/xray/config.json
 rm -f /etc/xray/$user-tls.json /etc/xray/$user-none.json
 fi
 done
+systemctl restart xray
 
 #----- Auto Remove Vless
 data=( `cat /etc/xray/config.json | grep '^#&' | cut -d ' ' -f 2 | sort | uniq`);
@@ -35,6 +36,7 @@ sed -i "/^#& $user $exp/,/^},{/d" /etc/xray/config.json
 sed -i "/^#& $user $exp/,/^},{/d" /etc/xray/config.json
 fi
 done
+systemctl restart xray
 
 #----- Auto Remove Trojan
 data=( `cat /etc/xray/config.json | grep '^#!' | cut -d ' ' -f 2 | sort | uniq`);
@@ -67,6 +69,22 @@ sed -i "/^#! $user $exp/,/^},{/d" /etc/trojan-go/config.json
 fi
 done
 systemctl restart trojan-go.service
+
+#----- Auto Remove Shadowsocks
+data=( `cat /etc/xray/config.json | grep '^##' | cut -d ' ' -f 2 | sort | uniq`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^## $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" -le "0" ]]; then
+sed -i "/^## $user $exp/,/^},{/d" /etc/xray/config.json
+sed -i "/^## $user $exp/,/^},{/d" /etc/xray/config.json
+fi
+done
+systemctl restart xray
 
 
 ##------ Auto Remove SSH
