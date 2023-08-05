97ba69cc6595aecdf6fc2bfc4e1c9c72a2537f8a
awanklod <137737752+awanklod@users.noreply.github.com>
2023-08-05 06:25:39 -1200

    Update limit.sh

--- limit/limit.sh
+++ limit/limit.sh
@@ -6,80 +6,80 @@ chmod + x /usr/local/sbin/quota
 cd /usr/local/sbin/
 sed -i 's/\r//' quota
 cd
-wget -q -O /usr/bin/limit-ip "${REPO}limit/limit-ip"
-chmod +x /usr/bin/*
-cd /usr/bin
-sed -i 's/\r//' limit-ip
-cd
-clear
+#wget -q -O /usr/bin/limit-ip "${REPO}limit/limit-ip"
+#chmod +x /usr/bin/*
+#cd /usr/bin
+#sed -i 's/\r//' limit-ip
+#cd
+#clear
 #SERVICE LIMIT ALL IP
-cat >/etc/systemd/system/vmip.service << EOF
-[Unit]
-Description=My
+#cat >/etc/systemd/system/vmip.service << EOF
+#[Unit]
+#Description=My
+#ProjectAfter=network.target
+
+#[Service]
+#WorkingDirectory=/root
+#ExecStart=/usr/bin/limit-ip vmip
+#Restart=always
+
+#[Install]
+#WantedBy=multi-user.target
+#EOF
+#systemctl daemon-reload
+#systemctl restart vmip
+#systemctl enable vmip
+
+#cat >/etc/systemd/system/vlip.service << EOF
+#[Unit]
+#Description=My
+#ProjectAfter=network.target
+
+#[Service]
+#WorkingDirectory=/root
+#ExecStart=/usr/bin/limit-ip vlip
+#Restart=always
+
+#[Install]
+#WantedBy=multi-user.target
+#EOF
+#systemctl daemon-reload
+#systemctl restart vlip
+#systemctl enable vlip
+
+#cat >/etc/systemd/system/trip.service << EOF
+#[Unit]
+#Description=My
+#ProjectAfter=network.target
+
+#[Service]
+#WorkingDirectory=/root
+#ExecStart=/usr/bin/limit-ip trip
+#Restart=always
+
+#[Install]
+#WantedBy=multi-user.target
+#EOF
+#systemctl daemon-reload
+#systemctl restart trip
+#systemctl enable trip
+
+#cat >/etc/systemd/system/shdip.service << EOF
+#[Unit]
+#Description=My
 ProjectAfter=network.target
 
-[Service]
-WorkingDirectory=/root
-ExecStart=/usr/bin/limit-ip vmip
-Restart=always
+#[Service]
+#WorkingDirectory=/root
+#ExecStart=/usr/bin/limit-ip shdip
+#Restart=always
 
-[Install]
-WantedBy=multi-user.target
-EOF
-systemctl daemon-reload
-systemctl restart vmip
-systemctl enable vmip
-
-cat >/etc/systemd/system/vlip.service << EOF
-[Unit]
-Description=My
-ProjectAfter=network.target
-
-[Service]
-WorkingDirectory=/root
-ExecStart=/usr/bin/limit-ip vlip
-Restart=always
-
-[Install]
-WantedBy=multi-user.target
-EOF
-systemctl daemon-reload
-systemctl restart vlip
-systemctl enable vlip
-
-cat >/etc/systemd/system/trip.service << EOF
-[Unit]
-Description=My
-ProjectAfter=network.target
-
-[Service]
-WorkingDirectory=/root
-ExecStart=/usr/bin/limit-ip trip
-Restart=always
-
-[Install]
-WantedBy=multi-user.target
-EOF
-systemctl daemon-reload
-systemctl restart trip
-systemctl enable trip
-
-cat >/etc/systemd/system/shdip.service << EOF
-[Unit]
-Description=My
-ProjectAfter=network.target
-
-[Service]
-WorkingDirectory=/root
-ExecStart=/usr/bin/limit-ip shdip
-Restart=always
-
-[Install]
-WantedBy=multi-user.target
-EOF
-systemctl daemon-reload
-systemctl restart shdip
-systemctl enable shdip
+#[Install]
+#WantedBy=multi-user.target
+#EOF
+#systemctl daemon-reload
+#systemctl restart shdip
+#systemctl enable shdip
 #SERVICE LIMIT QUOTA
 
 #SERVICE VMESS
