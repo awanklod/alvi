9c7e2a31c97e2cdfe197fed7e81d6874e301f5f6
awanklod <137737752+awanklod@users.noreply.github.com>
2023-08-05 06:45:01 -1200

    Update cek-vless.sh

--- xray/cek-vless.sh
+++ xray/cek-vless.sh
@@ -17,6 +17,31 @@ ORANGE='\033[0;33m'
 LIGHT='\033[0;37m'
 grenbo="\e[92;1m"
 clear
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+CEKEXPIRED () {
+today=$(date -d +1day +%Y-%m-%d)
+Exp1=$(curl -sS https://raw.githubusercontent.com/awanklod/izin_alvi/main/izin | grep $MYIP | awk '{print $3}')
+if [[ $today < $Exp1 ]]; then
+echo -e "\e[32mSTATUS SCRIPT AKTIF...\e[0m"
+else
+echo -e "\e[31mSCRIPT ANDA EXPIRED!\e[0m";
+    
+exit 0
+fi
+}
+IZIN=$(curl -sS https://raw.githubusercontent.com/awanklod/izin_alvi/main/izin | awk '{print $4}' | grep $MYIP)
+if [ $MYIP = $IZIN ]; then
+echo -e "\e[32mPermission Accepted...\e[0m"
+CEKEXPIRED
+else
+echo -e "\e[31mPermission Denied!\e[0m";
+
+exit 0
+fi
+
+
+clear
 function con() {
     local -i bytes=$1;
     if [[ $bytes -lt 1024 ]]; then
