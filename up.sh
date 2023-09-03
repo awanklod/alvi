ff71fdafa9fa27d143d95beef043bb1fbd523ce5
awanklod <137737752+awanklod@users.noreply.github.com>
2023-09-03 06:56:49 -1200

    Update up.sh

--- up.sh
+++ up.sh
@@ -18,6 +18,34 @@ rm -rf /etc/systemd/system/ws-stunnel.service
 rm -rf /etc/systemd/system/edu-proxy.service
 
 curl "https://raw.githubusercontent.com/awanklod/alvi3/main/sshws/insshws.sh" | bash
+
+wget -O /usr/local/bin/ws-openssh https://raw.githubusercontent.com/awanklod/alvi3/main/sshws/openssh-socket2.py
+wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/awanklod/alvi3/main/sshws/dropbear-ws2.py
+wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/awanklod/alvi3/main/sshws/ws-stunnel2
+wget -O /usr/local/bin/edu-proxy https://raw.githubusercontent.com/awanklod/alvi3/main/sshws/https.py
+
+systemctl daemon-reload
+Enable & Start & Restart ws-openssh service
+systemctl enable ws-openssh.service
+systemctl start ws-openssh.service
+systemctl restart ws-openssh.service
+
+#Enable & Start & Restart ws-dropbear service
+systemctl enable ws-dropbear.service
+systemctl start ws-dropbear.service
+systemctl restart ws-dropbear.service
+
+#Enable & Start & Restart ws-openssh service
+systemctl enable ws-stunnel.service
+systemctl start ws-stunnel.service
+systemctl restart ws-stunnel.service
+
+#Enable & Start & Restart directly dropbear
+systemctl daemon-reload
+systemctl enable edu-proxy.service
+systemctl start edu-proxy.service
+systemctl restart edu-proxy.service
+
 echo -e " [INFO] Update Successfully"
 sleep 2
 menu
