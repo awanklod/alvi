95b512e329f523229a8fe8ccfffd650114448ce4
awanklod <137737752+awanklod@users.noreply.github.com>
2023-09-03 06:57:33 -1200

    Update up.sh

--- up.sh
+++ up.sh
@@ -17,12 +17,12 @@ rm -rf /etc/systemd/system/ws-dropbear.service
 rm -rf /etc/systemd/system/ws-stunnel.service
 rm -rf /etc/systemd/system/edu-proxy.service
 
-curl "https://raw.githubusercontent.com/awanklod/alvi3/main/sshws/insshws.sh" | bash
+curl "https://raw.githubusercontent.com/awanklod/alvi/main/sshws/insshws.sh" | bash
 
-wget -O /usr/local/bin/ws-openssh https://raw.githubusercontent.com/awanklod/alvi3/main/sshws/openssh-socket2.py
-wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/awanklod/alvi3/main/sshws/dropbear-ws2.py
-wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/awanklod/alvi3/main/sshws/ws-stunnel2
-wget -O /usr/local/bin/edu-proxy https://raw.githubusercontent.com/awanklod/alvi3/main/sshws/https.py
+wget -O /usr/local/bin/ws-openssh https://raw.githubusercontent.com/awanklod/alvi/main/sshws/openssh-socket2.py
+wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/awanklod/alvi/main/sshws/dropbear-ws2.py
+wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/awanklod/alvi/main/sshws/ws-stunnel2
+wget -O /usr/local/bin/edu-proxy https://raw.githubusercontent.com/awanklod/alvi/main/sshws/https.py
 
 systemctl daemon-reload
 Enable & Start & Restart ws-openssh service
