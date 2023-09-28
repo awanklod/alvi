ba50d6778748ed975bfd00ae110c0dff12eebad7
awanklod <137737752+awanklod@users.noreply.github.com>
2023-09-28 00:15:29 -1200

    Update menu-ssws.sh

--- menu/menu-ssws.sh
+++ menu/menu-ssws.sh
@@ -47,8 +47,8 @@ echo -e "${BICyan}[${BIWhite}02${BICyan}]${RED} • ${NC}${BIWhite}TRIAL SHADOWS
 echo -e "${BICyan}[${BIWhite}03${BICyan}]${RED} • ${NC}${BIWhite}EXTEND SHADOWSOCKS  $NC"
 echo -e "${BICyan}[${BIWhite}04${BICyan}]${RED} • ${NC}${BIWhite}DELETE SHADOWSOCKS  $NC"
 echo -e "${BICyan}[${BIWhite}05${BICyan}]${RED} • ${NC}${BIWhite}CHECK SHADOWSOCKS  $NC"
-#echo -e "${BICyan}[${BIWhite}06${BICyan}]${RED} • ${NC}${BIWhite}CHECK SHADOWSOCKS 2  $NC"
-#echo -e "${BICyan}[${BIWhite}07${BICyan}]${RED} • ${NC}${BIWhite}CHECK SHADOWSOCKS 3  $NC"
+echo -e "${BICyan}[${BIWhite}06${BICyan}]${RED} • ${NC}${BIWhite}CHECK SHADOWSOCKS 2  $NC"
+echo -e "${BICyan}[${BIWhite}07${BICyan}]${RED} • ${NC}${BIWhite}CHECK SHADOWSOCKS 3  $NC"
 
 echo -e "${CYAN}╘════════════════════════════════════════╛${NC}"
 echo -e ""
@@ -66,8 +66,8 @@ case $opt in
 3) clear ; renew-ssws ;;
 4) clear ; del-ssws ;;
 5) clear ; cek-ssws ;;
-#6) clear ; cek-ssws2 ;;
-#7) clear ; cek-ssws3 ;;
+6) clear ; cek-ssws2 ;;
+7) clear ; cek-ssws3 ;;
 
 0) clear ; menu ;;
 x) exit ;;
