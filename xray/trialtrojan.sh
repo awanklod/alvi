675c0275e896f4ae43bcf7c3b96a5e1a7fdc87d5
awanklod <137737752+awanklod@users.noreply.github.com>
2023-09-30 02:38:42 -1200

    Update trialtrojan.sh

--- xray/trialtrojan.sh
+++ xray/trialtrojan.sh
@@ -86,8 +86,14 @@ trojanlink1="trojan://${uuid}@isi_bug_disini:${tls}?mode=gun&security=tls&type=g
 trojanlink="trojan://${uuid}@isi_bug_disini:${tls}?path=%2Ftrojan-ws&security=tls&host=${domain}&type=ws&sni=${domain}#${user}"
 trojanlink2="trojan://${uuid}@isi_bug_disini:${ntls}?path=%2Ftrojan-ws&security=tls&host=${domain}&type=ws#${user}"
 
-if [ ! -e /etc/trojan ]; then
-  mkdir -p /etc/trojan
+#if [ ! -e /etc/trojan ]; then
+#  mkdir -p /etc/trojan
+#fi
+
+if [[ $quota -gt 0 ]]; then
+echo -e "$[$quota * 1024 * 1024 * 1024]" > /etc/kyt/limit/trojan/quota/$user
+else
+echo > /dev/null
 fi
 
 if [[ $iplimit -gt 0 ]]; then
@@ -97,21 +103,21 @@ else
 echo > /dev/null
 fi
 
-if [ -z ${Quota} ]; then
-  Quota="0"
-fi
+#if [ -z ${Quota} ]; then
+ # Quota="0"
+#fi
 
-c=$(echo "${Quota}" | sed 's/[^0-9]*//g')
-d=$((${c} * 1024 * 1024 * 1024))
+#c=$(echo "${Quota}" | sed 's/[^0-9]*//g')
+#d=$((${c} * 1024 * 1024 * 1024))
 
-if [[ ${c} != "0" ]]; then
-  echo "${d}" >/etc/trojan/${user}
-fi
-DATADB=$(cat /etc/trojan/.trojan.db | grep "^###" | grep -w "${user}" | awk '{print $2}')
-if [[ "${DATADB}" != '' ]]; then
-  sed -i "/\b${user}\b/d" /etc/trojan/.trojan.db
-fi
-echo "### ${user} ${exp} ${uuid} ${Quota} ${iplimit}" >>/etc/trojan/.trojan.db
+#if [[ ${c} != "0" ]]; then
+#  echo "${d}" >/etc/trojan/${user}
+#fi
+#DATADB=$(cat /etc/trojan/.trojan.db | grep "^###" | grep -w "${user}" | awk '{print $2}')
+#if [[ "${DATADB}" != '' ]]; then
+#  sed -i "/\b${user}\b/d" /etc/trojan/.trojan.db
+#fi
+#echo "### ${user} ${exp} ${uuid} ${Quota} ${iplimit}" >>/etc/trojan/.trojan.db
 #echo "#! ${user} ${exp} ${uuid} ${Quota} ${iplimit}" >>/etc/trojan/.trojan.db
 clear
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
