9f146781f951c4217ff2b2b5493bc1158181fb7a
awanklod <137737752+awanklod@users.noreply.github.com>
2023-09-30 02:37:38 -1200

    Update trialssws.sh

--- xray/trialssws.sh
+++ xray/trialssws.sh
@@ -90,8 +90,13 @@ shadowsockslink2="ss://${shadowsocks_base64e}@isi_bug_disini:$ntls?path=ss-ws&se
 shadowsockslink1="ss://${shadowsocks_base64e}@${domain}:$tls?mode=gun&security=tls&type=grpc&serviceName=ss-grpc&sni=bug.com#${user}"
 systemctl restart xray > /dev/null 2>&1
 service cron restart > /dev/null 2>&1
-if [ ! -e /etc/shadowsocks ]; then
-  mkdir -p /etc/shadowsocks
+#if [ ! -e /etc/shadowsocks ]; then
+#  mkdir -p /etc/shadowsocks
+#fi
+if [[ $quota -gt 0 ]]; then
+echo -e "$[$quota * 1024 * 1024 * 1024]" > /etc/kyt/limit/shadowsocks/quota/$user
+else
+echo > /dev/null
 fi
 
 if [[ $iplimit -gt 0 ]]; then
@@ -101,21 +106,21 @@ else
 echo > /dev/null
 fi
 
-if [ -z ${Quota} ]; then
-  Quota="0"
-fi
+#if [ -z ${Quota} ]; then
+#  Quota="0"
+#fi
 
-c=$(echo "${Quota}" | sed 's/[^0-9]*//g')
-d=$((${c} * 1024 * 1024 * 1024))
+#c=$(echo "${Quota}" | sed 's/[^0-9]*//g')
+#d=$((${c} * 1024 * 1024 * 1024))
 
-if [[ ${c} != "0" ]]; then
-  echo "${d}" >/etc/shadowsocks/${user}
-fi
-DATADB=$(cat /etc/shadowsocks/.shadowsocks.db | grep "^###" | grep -w "${user}" | awk '{print $2}')
-if [[ "${DATADB}" != '' ]]; then
-  sed -i "/\b${user}\b/d" /etc/shadowsocks/.shadowsocks.db
-fi
-echo "### ${user} ${exp} ${uuid} ${Quota} ${iplimit}" >>/etc/shadowsocks/.shadowsocks.db
+#if [[ ${c} != "0" ]]; then
+#  echo "${d}" >/etc/shadowsocks/${user}
+#fi
+#DATADB=$(cat /etc/shadowsocks/.shadowsocks.db | grep "^###" | grep -w "${user}" | awk '{print $2}')
+#if [[ "${DATADB}" != '' ]]; then
+#  sed -i "/\b${user}\b/d" /etc/shadowsocks/.shadowsocks.db
+#fi
+#echo "### ${user} ${exp} ${uuid} ${Quota} ${iplimit}" >>/etc/shadowsocks/.shadowsocks.db
 #echo "## ${user} ${exp} ${uuid} ${Quota} ${iplimit}" >>/etc/shadowsocks/.shadowsocks.db
 
 clear
