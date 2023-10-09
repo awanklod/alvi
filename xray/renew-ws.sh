6487fbcb230257e15febae1b5deb23a0332b0445
awanklod <137737752+awanklod@users.noreply.github.com>
2023-10-09 03:39:25 -1200

    Update renew-ws.sh

--- xray/renew-ws.sh
+++ xray/renew-ws.sh
@@ -66,7 +66,7 @@ exit 0
 fi
 
 clear
-NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
+NUMBER_OF_CLIENTS=$(grep -c -E "^#vm# " "/etc/xray/config.json")
 if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 clear
 echo -e "${CYAN}╒════════════════════════════════════════╕${NC}"
@@ -86,7 +86,7 @@ echo -e "${CYAN}╒════════════════════�
 echo -e "${BIWhite}            ⇱ RENEW USER VMESS ⇲         ${NC}"
 echo -e "${CYAN}╘════════════════════════════════════════╛${NC}"
 echo ""
-grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
+grep -E "^#vm# " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
 echo ""
 red "tap enter to go back"
 echo -e "${CYAN}══════════════════════════════════════════${NC}"
@@ -95,14 +95,14 @@ if [ -z $user ]; then
 menu-vmess
 else
 read -p "Expired (days): " masaaktif
-exp=$(grep -wE "^### $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+exp=$(grep -wE "^#vm# $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
 now=$(date +%Y-%m-%d)
 d1=$(date -d "$exp" +%s)
 d2=$(date -d "$now" +%s)
 exp2=$(( (d1 - d2) / 86400 ))
 exp3=$(($exp2 + $masaaktif))
 exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
-sed -i "/### $user/c\### $user $exp4" /etc/xray/config.json
+sed -i "/#vm# $user/c\#vm# $user $exp4" /etc/xray/config.json
 systemctl restart xray > /dev/null 2>&1
 clear
 echo -e "${CYAN}══════════════════════════════════════════${NC}"
