a6ce70c2edbbb7e0c066c16f42771680ce106161
awanklod <137737752+awanklod@users.noreply.github.com>
2023-10-09 03:41:24 -1200

    Update xp.sh

--- ssh/xp.sh
+++ ssh/xp.sh
@@ -6,17 +6,17 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 
 
 ##----- Auto Remove Vmess
-data=( `cat /etc/xray/config.json | grep '^###' | cut -d ' ' -f 2 | sort | uniq`);
+data=( `cat /etc/xray/config.json | grep '^#vm#' | cut -d ' ' -f 2 | sort | uniq`);
 now=`date +"%Y-%m-%d"`
 for user in "${data[@]}"
 do
-exp=$(grep -w "^### $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+exp=$(grep -w "^#vm# $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
 d1=$(date -d "$exp" +%s)
 d2=$(date -d "$now" +%s)
 exp2=$(( (d1 - d2) / 86400 ))
 if [[ "$exp2" -le "0" ]]; then
-sed -i "/^### $user $exp/,/^},{/d" /etc/xray/config.json
-sed -i "/^### $user $exp/,/^},{/d" /etc/xray/config.json
+sed -i "/^#vm# $user $exp/,/^},{/d" /etc/xray/config.json
+sed -i "/^#vm# $user $exp/,/^},{/d" /etc/xray/config.json
 rm -f /etc/xray/$user-tls.json /etc/xray/$user-none.json
 fi
 done
