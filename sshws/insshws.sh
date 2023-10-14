752e4204aedb38e4555afa09dda55151623606f9
awanklod <137737752+awanklod@users.noreply.github.com>
2023-10-14 04:18:16 -1200

    Update insshws.sh

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -1,1 +1,36 @@
-a
+#!/bin/bash
+#installer Websocker tunneling 
+
+cd
+apt install python -y
+
+#Install Script Websocket-SSH Python
+wget -O /usr/local/bin/ws-dropbear http://sgpx.cybervpn.site:81/Autoscript-by-azi-main/Insshws/dropbear-ws.py.txt
+wget -O /usr/local/bin/ws-stunnel http://sgpx.cybervpn.site:81/Autoscript-by-azi-main/Insshws/ws-stunnel.txt
+
+#izin permision
+chmod +x /usr/local/bin/ws-dropbear
+chmod +x /usr/local/bin/ws-stunnel
+
+
+#System Dropbear Websocket-SSH Python
+wget -O /etc/systemd/system/ws-dropbear.service http://sgpx.cybervpn.site:81/Autoscript-by-azi-main/Insshws/service-wsdropbear.txt && chmod +x /etc/systemd/system/ws-dropbear.service
+
+#System SSL/TLS Websocket-SSH Python
+wget -O /etc/systemd/system/ws-stunnel.service http://sgpx.cybervpn.site:81/Autoscript-by-azi-main/Insshws/ws-stunnel.service.txt && chmod +x /etc/systemd/system/ws-stunnel.service
+
+#restart service
+#
+systemctl daemon-reload
+
+#Enable & Start & Restart ws-dropbear service
+systemctl enable ws-dropbear.service
+systemctl start ws-dropbear.service
+systemctl restart ws-dropbear.service
+
+#Enable & Start & Restart ws-openssh service
+systemctl enable ws-stunnel.service
+systemctl start ws-stunnel.service
+systemctl restart ws-stunnel.service
+
+clear
