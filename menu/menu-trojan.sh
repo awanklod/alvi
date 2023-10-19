97a30f88451eb3f082308ebf27252b3efc1f756e
awanklod <137737752+awanklod@users.noreply.github.com>
2023-10-19 10:15:48 -1200

    Update menu-trojan.sh

--- menu/menu-trojan.sh
+++ menu/menu-trojan.sh
@@ -117,9 +117,9 @@ echo -e "     ${BICyan}[${BIWhite}01${BICyan}] Create Account Trojan    "
 echo -e "     ${BICyan}[${BIWhite}02${BICyan}] Trial Account Trojan     "
 echo -e "     ${BICyan}[${BIWhite}03${BICyan}] Extending Account Trojan Active Life      "
 echo -e "     ${BICyan}[${BIWhite}04${BICyan}] Delete Account Trojan     "
-echo -e "     ${BICyan}[${BIWhite}05${BICyan}] Check User Login Trojan     "
+#echo -e "     ${BICyan}[${BIWhite}05${BICyan}] Check User Login Trojan     "
 echo -e "     ${BICyan}[${BIWhite}06${BICyan}] Check User Trojan     "
-echo -e "     ${BICyan}[${BIWhite}07${BICyan}] Check User Trojan 02     "
+#echo -e "     ${BICyan}[${BIWhite}07${BICyan}] Check User Trojan 02     "
 
 echo -e " ${BICyan}└─────────────────────────────────────────────────────┘${NC}"
 echo -e "     ${BIYellow}Press x or [ Ctrl+C ] • To-${BIWhite}Exit${NC}"
@@ -131,9 +131,9 @@ case $opt in
 2) clear ; trialtrojan ;;
 3) clear ; renew-tr ;;
 4) clear ; del-tr ;;
-5) clear ; cek-tr ;;
+#5) clear ; cek-tr ;;
 6) clear ; cek-tr2 ;;
-7) clear ; cek-tr3 ;;
+#7) clear ; cek-tr3 ;;
 
 0) clear ; menu ;;
 x) exit ;;
