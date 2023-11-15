2c61c887596845f20b793ffad7f98131a44bef4f
awanklod <137737752+awanklod@users.noreply.github.com>
2023-11-14 14:59:50 -1200

    Update limit.sh

--- limit/limit.sh
+++ limit/limit.sh
@@ -1,7 +1,7 @@
 REPO="https://raw.githubusercontent.com/awanklod/alvi/main/"
 print_install "Memasang Service Limit Quota"
-#wget -q -O /usr/local/sbin/quota "${REPO}limit/quota"
-wget -q -O /usr/local/sbin/quota "${REPO}limit/quota2"
+wget -q -O /usr/local/sbin/quota "${REPO}limit/quota"
+#wget -q -O /usr/local/sbin/quota "${REPO}limit/quota2"
 #wget -q -O /usr/local/sbin/quota "${REPO}limit/quota3"
 chmod +x /usr/local/sbin/quota
 chmod + x /usr/local/sbin/quota
