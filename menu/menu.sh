d13450918bc25a299b251fb87e9db20852e2c264
awanklod <137737752+awanklod@users.noreply.github.com>
2023-11-17 23:17:44 -1200

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -249,10 +249,13 @@ export pak=$( cat /home/.ver)
 IPVPS=$(curl -s ipinfo.io/ip )
 clear
 #figlet  PREMIUM  | lolcat
-echo -e "${BIPurple}╔══════════════════════════════════════════════════════════╗\033[0m${NC}"
-echo -e "${BIRed}                       CLOUDVPN  TUNNEL                      ${NC}"
-echo -e "${BIPurple}╚══════════════════════════════════════════════════════════╝\033[0m${NC}"
-echo -e "${BIBlack}╔════════════════════════════════════════════════════════════╗\033[0m${NC}"
+#echo -e "${BIPurple}╔══════════════════════════════════════════════════════════╗\033[0m${NC}"
+#echo -e "${BIRed}                       CLOUDVPN  TUNNEL                      ${NC}"
+#echo -e "${BIPurple}╚══════════════════════════════════════════════════════════╝\033[0m${NC}"
+echo -e "\033[1;93m──────────────────────────────────────────\033[0m"
+echo -e "\033[42m    ∆ INFORMATION AUTOSCRIPT VPS ∆               \033[0m"
+echo -e "\033[1;93m──────────────────────────────────────────\033[0m"
+echo -e "${BIYellow}╔════════════════════════════════════════════════════════════╗\033[0m${NC}"
 echo -e "${BIYellow}║ Server Uptime       = ${BIWhite}$( uptime -p  | cut -d " " -f 2-10000 ) ${NC}"
 echo -e "${BIYellow}║ Current Time        = ${BIWhite}$( date -d "0 days" +"%d-%m-%Y | %X" )${NC}"
 echo -e "${BIYellow}║ Operating System    = ${BIWhite}$( cat /etc/os-release | grep -w PRETTY_NAME | sed 's/PRETTY_NAME//g' | sed 's/=//g' | sed 's/"//g' ) ( $( uname -m) )${NC}"
@@ -261,7 +264,7 @@ echo -e "${BIYellow}║ Current Domain      = ${BIWhite}$( cat /etc/xray/domain 
 echo -e "${BIYellow}║ NS Domain           = ${BIWhite}$(cat /root/nsdomain)${NC}"
 echo -e "${BIYellow}║ RAM/USE RAM % CPU   = ${BIWhite}${totalram}MB / ${BIWhite}${USAGERAM} MB & ${BIWhite}${cpu_usage}${NC}"
 #echo -e "${BIYellow}║ CPU Usage           = ${BIWhite}${cpu_usage}${NC}"
-echo -e "${BIBlack}╚════════════════════════════════════════════════════════════╝\033[0m${NC}"
+echo -e "${BIYellow}╚════════════════════════════════════════════════════════════╝\033[0m${NC}"
 DATE=$(date +'%d %B %Y')
 datediff() {
     d1=$(date -d "$1" +%s)
