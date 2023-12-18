9872d7c3d2060dd80519c673281ab0f90b696880
awanklod <137737752+awanklod@users.noreply.github.com>
2023-12-18 04:00:54 -1200

    Update trialvless.sh

--- xray/trialvless.sh
+++ xray/trialvless.sh
@@ -73,7 +73,7 @@ user=trial-vl`</dev/urandom tr -dc 0-9 | head -c4`
 uuid=$(cat /proc/sys/kernel/random/uuid)
 masaaktif=1
 Quota=5
-#iplimit=1
+iplimit=1
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 sed -i '/#vless$/a\#& '"$user $exp"'\
 },{"id": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
@@ -94,12 +94,12 @@ fi
 #echo > /dev/null
 #fi
 
-#if [[ $iplimit -gt 0 ]]; then
-#mkdir -p /etc/kyt/limit/vless/ip
-#echo -e "$iplimit" > /etc/kyt/limit/vless/ip/$user
-#else
-#echo > /dev/null
-#fi
+if [[ $iplimit -gt 0 ]]; then
+mkdir -p /etc/kyt/limit/vless/ip
+echo -e "$iplimit" > /etc/kyt/limit/vless/ip/$user
+else
+echo > /dev/null
+fi
 
 if [ -z ${Quota} ]; then
   Quota="0"
@@ -115,8 +115,8 @@ DATADB=$(cat /etc/vless/.vless.db | grep "^#&" | grep -w "${user}" | awk '{print
 if [[ "${DATADB}" != '' ]]; then
 sed -i "/\b${user}\b/d" /etc/vless/.vless.db
 fi
-#echo "#& ${user} ${exp} ${uuid} ${Quota} ${iplimit}" >>/etc/vless/.vless.db
-echo "#& ${user} ${exp} ${uuid} ${Quota}" >>/etc/vless/.vless.db
+echo "#& ${user} ${exp} ${uuid} ${Quota} ${iplimit}" >>/etc/vless/.vless.db
+#echo "#& ${user} ${exp} ${uuid} ${Quota}" >>/etc/vless/.vless.db
 clear
 echo -e "${CYAN}╒════════════════════════════════════════╕${NC}" 
 echo -e "${BIWhite}            ⇱ VLESS ACCOUNT ⇲            ${NC}"
@@ -124,7 +124,7 @@ echo -e "${CYAN}╘════════════════════�
 echo -e "Remarks        : ${user}"
 echo -e "Domain         : ${domain}"
 echo -e "User limit     : ${Quota} GB"
-#echo -e "Ip   limit     : ${iplimit} Ip"
+echo -e "Ip   limit     : ${iplimit} Ip"
 echo -e "Wildcard       : (bug.com).${domain}"
 echo -e "Port TLS       : 443"
 echo -e "Port none TLS  : 80"
