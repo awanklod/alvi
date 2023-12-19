c348a97436e95ab5643138a3887492d1dac5f44b
awanklod <137737752+awanklod@users.noreply.github.com>
2023-12-19 05:58:32 -1200

    Update editip-vmess.sh

--- xray/editip-vmess.sh
+++ xray/editip-vmess.sh
@@ -65,43 +65,43 @@ red "Permission Denied!"
 exit 0
 fi
 clear
-NUMBER_OF_CLIENTS=$(grep -c -E "^#vm# " "/etc/vmess/.vmess.db")
+NUMBER_OF_CLIENTS=$(grep -c -E "^#vmn# " "/etc/vmess/.vmess.db")
         if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
                 clear
-echo -e "${CYAN}╒════════════════════════════════════════╕${NC}"
-echo -e "${BIWhite}               ⇱ VMESS MENU ⇲            ${NC}"
-echo -e "${CYAN}╘════════════════════════════════════════╛${NC}"
+        echo -e "\033[0;33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+        echo -e "           Edit Limit vmess          \E[0m"
+        echo -e "\033[0;33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
                 echo ""
                 echo "You have no existing clients!"
                 echo ""
-                echo -e "\033[35m══════════════════════════════════════════\e[0m"
+                echo -e "\033[0;33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
         echo ""
         read -n 1 -s -r -p "Press any key to back on menu"
         menu
         fi
 
         clear
-echo -e "${CYAN}╒════════════════════════════════════════╕${NC}"
-echo -e "${BIWhite}               ⇱ VMESS MENU ⇲            ${NC}"
-echo -e "${CYAN}╘════════════════════════════════════════╛${NC}"
+        echo -e "\033[0;33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo -e "           Edit Limit vmess          \E[0m"
+    echo -e "\033[0;33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo ""
         grep -E "^#vm# " "/etc/vmess/.vmess.db" | cut -d ' ' -f 2 | column -t | sort | uniq
     echo ""
     red "tap enter to go back"
-    echo -e "\033[35m══════════════════════════════════════════\e[0m"
+    echo -e "\033[0;33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
         read -rp "Input Username : " user
     if [ -z $user ]; then
     menu
     else
-    read -p "Limit (Quota): " Quota
-    echo -e "$[$Quota * 1024 * 1024 * 1024]" > /etc/vmess/${user}
+    read -p "Limit (IP): " iplimit
+    echo -e "${iplimit}" > /etc/kyt/limit/vmess/ip/${user}
     clear
     echo -e "\033[0;33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-    echo " VMESS Account Was Successfully Edited"
+    echo " vmess Account Was Successfully Edited"
     echo -e "\033[0;33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo ""
     echo " Client Name  : $user"
-    echo " Quota Ready  : $Quota GB"
+    echo " Limit IP Ready  : $iplimit IP"
     echo ""
     echo -e "\033[0;33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo ""
