c617f8619a875a1859090f5fc96751eb7d1a08ef
awanklod <137737752+awanklod@users.noreply.github.com>
2023-12-19 12:46:07 -1200

    Update cek-ws.sh

--- xray/cek-ws.sh
+++ xray/cek-ws.sh
@@ -79,22 +79,20 @@ function con() {
     fi
 }
 echo -n > /tmp/other.txt
-data=( `cat /etc/xray/config.json | grep '#vm#' | cut -d ' ' -f 2 | sort | uniq`);
-echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
-echo -e "              VMESS USER LOGIN            $NC"
-echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
-echo -e "   User"     "       Last Login"    "  Usage"   " Total IP"
-echo -e "${CYAN}╒════════════════════════════════════════╕${NC}"
+data=( `cat /etc/xray/config.json | grep '^#vm#' | cut -d ' ' -f 2`);
+echo "----------------------------------------";
+echo "---------=[ Vmess User Login ]=---------";
+echo "----------------------------------------";
 for akun in "${data[@]}"
 do
 if [[ -z "$akun" ]]; then
-akun="tidakada"
+akun="There isn't any"
 fi
 echo -n > /tmp/ipvmess.txt
-data2=( `cat /var/log/xray/access.log | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | sort | uniq`);
+data2=( `netstat -anp | grep ESTABLISHED | grep tcp6 | grep xray | awk '{print $5}' | cut -d: -f1 | sort | uniq`);
 for ip in "${data2[@]}"
 do
-jum=$(cat /var/log/xray/access.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | grep -w "$ip" | sort | uniq)
+jum=$(cat /var/log/xray/access.log | grep -w $akun | awk '{print $3}' | cut -d: -f1 | grep -w $ip | sort | uniq)
 if [[ "$jum" = "$ip" ]]; then
 echo "$jum" >> /tmp/ipvmess.txt
 else
@@ -107,16 +105,17 @@ jum=$(cat /tmp/ipvmess.txt)
 if [[ -z "$jum" ]]; then
 echo > /dev/null
 else
-jum2=$(cat /tmp/ipvmess.txt | wc -l)
-byte=$(cat /etc/vmess/${akun})
-lim=$(con ${byte})
-wey=$(cat /etc/limit/vmess/${akun})
-gb=$(con ${wey})
-lastlogin=$(cat /var/log/xray/access.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 2 | tail -1)
-printf "  %-13s %-7s %-8s %2s\n"   "${akun}" "$lastlogin"  " ${gb}/${lim}"   "$jum2";
-fi 
+jum2=$(cat /tmp/ipvmess.txt | nl)
+echo "user : $akun";
+echo "$jum2";
+echo "----------------------------------------"
+fi
 rm -rf /tmp/ipvmess.txt
 done
+oth=$(cat /tmp/other.txt | sort | uniq | nl)
+echo "other";
+echo "$oth";
+echo "----------------------------------------"
 rm -rf /tmp/other.txt
 echo ""
 echo -e "${CYAN}╒════════════════════════════════════════╕${NC}"
