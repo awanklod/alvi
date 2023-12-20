f2efcfb796608d2a6ea22a26e8c7e7f730be8378
awanklod <137737752+awanklod@users.noreply.github.com>
2023-12-19 12:47:17 -1200

    Update cek-tr.sh

--- xray/cek-tr.sh
+++ xray/cek-tr.sh
@@ -30,22 +30,20 @@ function con() {
     fi
 }
 echo -n > /tmp/other.txt
-data=( `cat /etc/xray/config.json | grep '^#!' | cut -d ' ' -f 2 | sort | uniq`);
-echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
-echo -e "              TROJAN USER LOGIN            $NC"
-echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
-echo -e "   User"     "       Last Login"    "  Usage"   " Total IP"
-echo -e "${CYAN}╒════════════════════════════════════════╕${NC}"
+data=( `cat /etc/xray/config.json | grep '^#&#' | cut -d ' ' -f 2`);
+echo "-----------------------------------------";
+echo "---------=[ Trojan User Login ]=---------";
+echo "-----------------------------------------";
 for akun in "${data[@]}"
 do
 if [[ -z "$akun" ]]; then
 akun="tidakada"
 fi
 echo -n > /tmp/iptrojan.txt
-data2=( `cat /var/log/xray/access.log | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | sort | uniq`);
+data2=( `netstat -anp | grep ESTABLISHED | grep tcp6 | grep xray | awk '{print $5}' | cut -d: -f1 | sort | uniq`);
 for ip in "${data2[@]}"
 do
-jum=$(cat /var/log/xray/access.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | grep -w "$ip" | sort | uniq)
+jum=$(cat /var/log/xray/access.log | grep -w $akun | awk '{print $3}' | cut -d: -f1 | grep -w $ip | sort | uniq)
 if [[ "$jum" = "$ip" ]]; then
 echo "$jum" >> /tmp/iptrojan.txt
 else
@@ -58,16 +56,17 @@ jum=$(cat /tmp/iptrojan.txt)
 if [[ -z "$jum" ]]; then
 echo > /dev/null
 else
-jum2=$(cat /tmp/iptrojan.txt | wc -l)
-byte=$(cat /etc/trojan/${akun})
-lim=$(con ${byte})
-wey=$(cat /etc/limit/trojan/${akun})
-gb=$(con ${wey})
-lastlogin=$(cat /var/log/xray/access.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 2 | tail -1)
-printf "  %-13s %-7s %-8s %2s\n"   "${akun}" "$lastlogin"  " ${gb}/${lim}"   "$jum2";
-fi 
+jum2=$(cat /tmp/iptrojan.txt | nl)
+echo "user : $akun";
+echo "$jum2";
+echo "-----------------------------------------"
+fi
 rm -rf /tmp/iptrojan.txt
 done
+oth=$(cat /tmp/other.txt | sort | uniq | nl)
+echo "other";
+echo "$oth";
+echo "-----------------------------------------"
 rm -rf /tmp/other.txt
 echo ""
 echo -e "${CYAN}╒════════════════════════════════════════╕${NC}"
