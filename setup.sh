08e4f187c4c4bfd44410168867656cedc160f74e
awanklod <137737752+awanklod@users.noreply.github.com>
2023-12-27 01:46:28 -1200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -199,10 +199,10 @@ echo -e "$green          Install LIMIT              $NC"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 sleep 2
 clear
-#wget https://raw.githubusercontent.com/awanklod/alvi/main/limit3/quota.sh && chmod +x quota.sh && ./quota.sh
-#wget https://raw.githubusercontent.com/awanklod/alvi/main/limit/limit-ipfinal.sh && chmod +x limit-ipfinal.sh && ./limit-ipfinal.sh
+wget https://raw.githubusercontent.com/awanklod/alvi/main/limit3/quota.sh && chmod +x quota.sh && ./quota.sh
+wget https://raw.githubusercontent.com/awanklod/alvi/main/limit/limit-ipfinal.sh && chmod +x limit-ipfinal.sh && ./limit-ipfinal.sh
 #wget https://raw.githubusercontent.com/awanklod/alvi/main/limit/limit-quotafinal.sh && chmod +x limit-quotafinal.sh && ./limit-quotafinal.sh
-wget https://raw.githubusercontent.com/awanklod/alvi/main/limit/limit.sh && chmod +x limit.sh && ./limit.sh
+#wget https://raw.githubusercontent.com/awanklod/alvi/main/limit/limit.sh && chmod +x limit.sh && ./limit.sh
 
 clear
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
